1952dd0986015b14dc54d0908ee5ff7e24ef9af7
James Gilbert <James.gilbert@nottingham.ac.uk>
2018-06-12 16:59:06 +0100

    Overlapping lfr for rwr

--- experiments/lfr_overlap.py
+++ experiments/lfr_overlap.py
@@ -3,7 +3,7 @@ HPC script for running evaluation of performance on benchmark graphs
 
 This script is supposed to be run by the jobscript
 """
-from experiments import get_benchmark, get_auc_scores_community
+from experiments import get_benchmark, get_auc_scores_community, get_auc_scores_community_rwr, lfr_benchmark_graph
 import numpy as np
 import click
 from subprocess import call
@@ -22,6 +22,7 @@ _base_params = dict(
 )
 
 _seed_sizes = [1, 3, 7, 15]
+
 
 @click.command()
 @click.argument("n")
@@ -49,6 +50,37 @@ def auc_compute(seed, n, ol, results_folder):
                 # Seed of AUC scores for node this size
                 scom = np.array([nmap[i] for i in comm])
                 auc_s = get_auc_scores_community(seed_size, scom, nodes, membership_ma)
+                results.append([int(n), int(ol), int(seed), c, seed_size, len(comm), np.mean(auc_s), np.std(auc_s)])
+
+    with open(results_file, "w+") as rf:
+        json.dump(results, rf)
+
+
+@click.command()
+@click.argument("n")
+@click.argument("ol")
+@click.argument("seed")
+@click.argument("results_folder")
+def auc_compute_rwr(seed, n, ol, results_folder):
+    pset = _base_params.copy()
+    pset['n'] = int(n)
+    pset['overlapping_nodes'] = ol
+    pset['seed'] = int(seed)
+
+    rp = "{}_{}_{}_rwr.json".format(n, ol, seed)
+    results_file = os.path.abspath(os.path.join(results_folder, rp))
+
+    graph, communities = lfr_benchmark_graph(**pset)
+
+    nmap = dict([(j, i) for i, j in enumerate(sorted(graph.nodes()))])
+
+    results = []
+    for c, comm in communities.items():
+        for seed_size in _seed_sizes:
+            if len(comm) > seed_size:
+                # Seed of AUC scores for node this size
+                scom = np.array([nmap[i] for i in comm])
+                auc_s = get_auc_scores_community_rwr(seed_size, scom, graph)
                 results.append([int(n), int(ol), int(seed), c, seed_size, len(comm), np.mean(auc_s), np.std(auc_s)])
 
     with open(results_file, "w+") as rf:
@@ -132,6 +164,81 @@ python {file} auc_compute {n} {ol} $JOB {results_folder}
 
 @click.command()
 @click.argument("n")
+@click.option("--network_samples", default=10)
+@click.option("--walltime", default="01:30:00")
+@click.option("--request", default="select=1:ncpus=16:mem=31gb")
+@click.option("--execute/--no_exec", default=False)
+@click.option("--queue", default="HPCA-01839-EFR")
+def run_jobs_rwr(n, network_samples, walltime, request, execute, queue):
+    script_template = """#!/bin/bash
+#PBS -k oe
+#PBS -l {request}
+#PBS -l {walltime}
+#PBS -P {queue}
+
+WORK_DIR={workdir}
+JOB=$PBS_ARRAY_INDEX
+
+cd $WORK_DIR
+python {file} auc_compute_rwr {n} {ol} $JOB {results_folder}
+
+"""
+    workdir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+    results_folder = os.path.join(workdir, 'hpc_results', "lfr_overlap_{}_rwr".format(n))
+
+    if not os.path.exists(results_folder):
+        os.makedirs(results_folder)
+
+    script_settings = dict(
+        walltime="walltime={}".format(walltime),
+        request=request,
+        queue=queue,
+        workdir=workdir,
+        results_folder=results_folder,
+        n=n,
+        file=os.path.abspath(__file__),
+    )
+
+    opt_path = os.path.join(workdir, 'hpc_out', 'output_{}'.format(n))
+    err_path = os.path.join(workdir, 'hpc_out', 'error_{}'.format(n))
+
+    os.makedirs(os.path.join(workdir, 'hpc_out'), exist_ok=True)
+
+    cmd_opt = dict(
+        options="",
+        jcount=network_samples,
+        n=n,
+        e=err_path,
+        o=opt_path,
+    )
+    cmd_template = "qsub {options} -J 1-{jcount} -e {e} -o {o} {command_file}"
+
+    commands_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'job_scripts'))
+    if not os.path.exists(commands_path):
+        os.mkdir(commands_path)
+
+    overlap_vals = np.linspace(0.0, 1.0, 10)
+    overlap_vals *= int(n)
+    for ol in overlap_vals.astype(int):
+        command_file_path = os.path.join(commands_path, "lfr_bm_overlap_{}_{:.2f}_rwr.sh".format(n, ol))
+
+        script_settings["ol"] = ol
+        script = script_template.format(**script_settings)
+
+        with open(command_file_path, "w+") as cmdfile:
+            cmdfile.write(script)
+
+        params = cmd_opt.copy()
+
+        params['command_file'] = command_file_path
+        command = cmd_template.format(**params)
+        click.echo(command)
+        if execute:
+            call(command.split())
+
+
+@click.command()
+@click.argument("n")
 def gen_figure(n):
     import json
     from matplotlib import pyplot as plt
@@ -183,5 +290,7 @@ def cli():
 if __name__ == "__main__":
     cli.add_command(run_jobs)
     cli.add_command(auc_compute)
+    cli.add_command(run_jobs_rwr)
+    cli.add_command(auc_compute_rwr)
     cli.add_command(gen_figure)
     cli()
